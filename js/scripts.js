47ed367573753a7e3f080c822bf58a83028f0727
Hassan Al-khalifah and William Wittenbrock <william@>
2018-08-13 10:11:16 -0700

    Add loop to input array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,40 @@
-var blanks = []
+var blanks = ["name", "favFood", "beverage"];
 
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
 
-    blanks += $("#name").val();
-    blanks += $("#favFood").val();
-    blanks += $("#beverage").val();
-    
-    console.log(name);
-  });
-});
+    // blanks.push($("#name").val());
+    // blanks.push($("#favFood").val());
+    // blanks.push($("#beverage").val());
+
+
+    blanks.forEach(function(blank) {
+      var userInput = $("#" + blank).val();
+
+
+      $ ("#output").append("<li>" + userInput + "</li>");
+    });
+        console.log(userInput);
+      });
+    });
+
+
+
+
+
+
+
+
+
+
+    // $ ("#output").append("<li>" + blanks[0] + "</li>");
+    // $ ("#output").append("<li>" + blanks[1] + "</li>");
+    // $ ("#output").append("<li>" + blanks[2] + "</li>");
+//
+// blanks.forEach(function(blank) {
+//   $ ("#output").append("<li>" + blank + "</li>");
+// });
+//     console.log(blanks);
+//   });
+// });
